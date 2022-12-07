69199934abab1720288c14426da704539a0ae4ad
redmont <deepanshusingh@192.168.1.34>
2022-12-07 22:51:32 +0530

    print formatted

--- sample-react-app/src/App.js
+++ sample-react-app/src/App.js
@@ -22,6 +22,7 @@ const fetchClient = new FetchNFTClient({openSeaConfig, solanaConfig});
 const App = () => {
     const [collectibles, setCollectibles] = useState(null);
     const [ethCollection, setEthCollection] = useState(null);
+    const [resultType, setResultType] = useState(null);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
@@ -45,36 +46,47 @@ const App = () => {
             <div className="Header">Eth Collectibles</div>
             <form
                 onSubmit={(e) => {
-                    console.log(e);
                     e.preventDefault();
                     fetchCollection(e.target[0]?.value);
                 }}
             >
                 <input
                     name="Collection"
+                    placeholder="address"
                     style={{width: 400, marginRight: 10, marginTop: 10}}
                 ></input>
                 <br></br>
                 <br></br>
-                <button type="submit">Render NFTs</button>
+                <button type="submit" onClick={() => setResultType("render")}>
+                    Render NFTs
+                </button>
                 {" or "}
-                <button type="submit">Print formatted NFTs info</button>
+                <button type="submit" onClick={() => setResultType("print")}>
+                    Print formatted NFTs info
+                </button>
             </form>
             <br></br>
             <br></br>
             {loading && "Loading..."}
-            {collectibles?.ethCollectibles[ethCollection]?.map((collectible) => (
-                <div className="Collectibles">
-                    <div className="Name">{collectible.name}</div>
-                    <img
-                        className="Image"
-                        src={collectible.frameUrl || collectible.gifUrl}
-                        alt={collectible.name}
-                    />
-                    <br></br>
-                    <br></br>
-                </div>
-            ))}
+            {resultType && resultType === "print" && (
+                <pre style={{textAlign: "left"}}>
+                    {JSON.stringify(collectibles?.ethCollectibles[ethCollection], null, "\t")}
+                </pre>
+            )}
+            {resultType &&
+                resultType === "render" &&
+                collectibles?.ethCollectibles[ethCollection]?.map((collectible) => (
+                    <div className="Collectibles">
+                        <div className="Name">{collectible.name}</div>
+                        <img
+                            className="Image"
+                            src={collectible.frameUrl || collectible.gifUrl}
+                            alt={collectible.name}
+                        />
+                        <br></br>
+                        <br></br>
+                    </div>
+                ))}
         </div>
     );
 };
